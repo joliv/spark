22fe1fe5c2383c40935548b4e14d5369ef111bef
joliv <joliv@users.noreply.github.com>
2014-06-12 23:29:12 -0400

    Added a couple more tests
    They're certainly not as resilient as the first one. May want to adjust them later.

--- spark_test.go
+++ spark_test.go
@@ -13,4 +13,14 @@ func TestSpark(t *testing.T) {
 	if x := Spark(in); x != out {
 		t.Errorf("Spark(%v) = %v, want %v", in, x, out)
 	}
+	in = []float64{1, 0, 0, 1}
+	out = "█▁▁█"
+	if x := Spark(in); x != out {
+		t.Errorf("Spark(%v) = %v, want %v", in, x, out)
+	}
+	in = []float64{67, 71, 77, 85, 95, 104, 106, 105, 100, 89, 76, 66}
+	out = "▁▂▃▄▆███▇▅▃▁"
+	if x := Spark(in); x != out {
+		t.Errorf("Spark(%v) = %v, want %v", in, x, out)
+	}
 }
