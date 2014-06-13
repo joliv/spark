088c86d9c95951e95abc5015501e7c23e2967345
joliv <joliv@users.noreply.github.com>
2014-06-12 22:39:30 -0400

    Created spark package

---
+++ spark.go
@@ -0,0 +1,58 @@
+package spark
+
+import (
+	"bytes"
+	"math"
+)
+
+var steps = []rune("▁▂▃▄▅▆▇█")
+
+func Spark(nums []float64) string {
+	indices := normalize(nums)
+	var sparkline bytes.Buffer
+	for _, index := range indices {
+		sparkline.WriteRune(steps[index])
+	}
+	return sparkline.String()
+}
+
+func normalize(nums []float64) []int {
+	var indices []int
+	min := minimum(nums)
+	for i, _ := range nums {
+		nums[i] -= min
+	}
+	max := maximum(nums)
+	for i, _ := range nums {
+		x := nums[i]
+		x /= max
+		x *= 8
+		if x == 8 {
+			x = 7
+		} else {
+			x = math.Floor(x)
+		}
+		indices = append(indices, int(x))
+	}
+	return indices
+}
+
+func minimum(nums []float64) float64 {
+	var min float64 = nums[0]
+	for _, x := range nums {
+		if math.Min(x, min) == x {
+			min = x
+		}
+	}
+	return min
+}
+
+func maximum(nums []float64) float64 {
+	var max float64 = nums[0]
+	for _, x := range nums {
+		if math.Max(x, max) == x {
+			max = x
+		}
+	}
+	return max
+}
